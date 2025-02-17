a95e0e6430ad09eb1c332805e85e4e6ca277044e
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-17 15:03:31 +0100

    add first snack

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function sayHi(name) {
+  return "ciao " + name;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const greeting = sayHi(userName);
+console.log(greeting);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
